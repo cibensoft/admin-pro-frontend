2f605cdf12420344fdd1f783e995fbe73bccf5eb
cibensoft <cibensoft@gmail.com>
2021-11-11 11:22:24 -0300

    Fin seccion 6

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedModule } from '../shared/shared.module';
 // Se puede importar el AppRoutingModule o impoprtar el RouterModule
 // import { AppRoutingModule } from '../app-routing.module';//Este modulo ya esta cargado en memoria ya que lo importe
 // //en app-module, asi que en este caso simplemente lo va a volver a utilizar, no es que lo importa dos veces
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+//Modulos propios
+import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from '../components/components.module';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -26,9 +30,11 @@ import { PagesComponent } from './pages.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     SharedModule,
     // AppRoutingModule
-    RouterModule
+    RouterModule,
+    ComponentsModule,
   ]
 })
 export class PagesModule { }
