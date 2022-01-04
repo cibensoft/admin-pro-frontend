f11711dfa7682e9febbe3b2850fb7dd392e5dbbb
cibensoft <cibensoft@gmail.com>
2022-01-04 11:07:03 -0300

    Environment terminado, lazyLoad

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -2,43 +2,16 @@ import { Routes, RouterModule } from "@angular/router";
 import { NgModule } from '@angular/core';
 
 import { AuthGuard } from "../guards/auth.guard";
-import { AdminGuard } from "../guards/admin.guard";
 
 import { PagesComponent } from "./pages.component";
-import { DashboardComponent } from "./dashboard/dashboard.component";
-import { ProgressComponent } from "./progress/progress.component";
-import { Grafica1Component } from "./grafica1/grafica1.component";
-import { AccountSettingsComponent } from "./account-settings/account-settings.component";
-import { PromesasComponent } from "./promesas/promesas.component";
-import { RxjsComponent } from "./rxjs/rxjs.component";
-import { PerfilComponent } from "./perfil/perfil.component";
-//Mantenimientos
-import { UsuariosComponent } from "./mantenimientos/usuarios/usuarios.component";
-import { HospitalesComponent } from "./mantenimientos/hospitales/hospitales.component";
-import { MedicosComponent } from "./mantenimientos/medicos/medicos.component";
-import { MedicoComponent } from "./mantenimientos/medicos/medico.component";
-import { BusquedaComponent } from "./busqueda/busqueda.component";
 
 const routes: Routes = [
     {
         path: 'dashboard',
         component: PagesComponent,
         canActivate: [AuthGuard],
-        children: [
-            { path: '', component: DashboardComponent, data: { titulo: 'Dashboard' } },
-            { path: 'progress', component: ProgressComponent, data: { titulo: 'ProgressBar' } },
-            { path: 'grafica1', component: Grafica1Component, data: { titulo: 'Grafica #1' } },
-            { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Ajustes de cuenta' } },
-            { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' } },
-            { path: 'rxjs', component: RxjsComponent, data: { titulo: 'RxJs' } },
-            { path: 'perfil', component: PerfilComponent, data: { titulo: 'Perfil de usuario' } },
-            //Mantenimientos
-            { path: 'usuarios', component: UsuariosComponent, data: { titulo: 'Mantenimiento de usuarios' }, canActivate: [AdminGuard] },
-            { path: 'hospitales', component: HospitalesComponent, data: { titulo: 'Mantenimiento de hospitales' } },
-            { path: 'medicos', component: MedicosComponent, data: { titulo: 'Mantenimiento de medicos' } },
-            { path: 'medico/:id', component: MedicoComponent, data: { titulo: 'Mantenimiento de medicos' } },
-            { path: 'buscar/:termino', component: BusquedaComponent, data: { titulo: 'Busquedas' } },
-        ],
+        canLoad: [AuthGuard],//es otro guard que se debe implementar si estamos trabajando con lazyLoad
+        loadChildren: () => import('./child-routes.module').then(m => m.ChildRoutesModule)
     },
 ];
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 
 export const environment = {
   production: true,
-  base_url: 'http://localhost:3000/api'
+  base_url: 'https://adminpro-curso-backend.herokuapp.com/api'
 };
